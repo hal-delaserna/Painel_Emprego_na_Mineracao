b703351aaf3f69930a56743a4552f8d0f6825354
DNPMNET\humberto.serna <humberto.laserna@gmail.com>
2022-12-18 21:17:46 -0300

    Tabela + Gráficos em tabBox cada. Mas talvez os gráficos tenham ficado com legendas pequenas... Depois vejo se coloco as Agregações, seja por UF ou subníveis CNAE, tudo em telas separadas: teria mais espaço pra rechear com detalhes...

--- server.R
+++ server.R
@@ -1,17 +1,84 @@
 library(shiny)
 
+
+
 shinyServer(function(input, output) {
+ 
+ 
+ df_Divisao <-
+   eventReactive(input$id.Atualizar.button, {
+     df_long_cnae_Divisao[df_long_cnae_Divisao$trimestre %in% 
+                    TRIMESTRES[
+                      as.integer(
+                        TRIMESTRES[TRIMESTRES == input$id.Trimestre.select[1]]
+                      ):as.integer(
+                        TRIMESTRES[TRIMESTRES == input$id.Trimestre.select[2]]
+                      )] &
+                    df_long_cnae_Divisao$UF_sigla %in% input$id.UF.select, ]
+   }, ignoreNULL = F)
+ 
+
+ 
+ df_Divisao_Wide <- 
+   reactive(
+     pivot_wider(df_Divisao(), 
+                 names_from = "trimestre", 
+                 values_from = "estoque")
+   )
+ 
+ 
+ df_Divisao_Wide <- 
+   reactive(
+     pivot_wider(df_Divisao(), 
+                 names_from = "trimestre", 
+                 values_from = "estoque")
+   )
+ 
+ 
+ 
+ output$id.Graf.Trimestre <-
+   renderPlot({
+     ggplot(data = df_Divisao(),
+            mapping = aes(x = trimestre, y = estoque)) +
+       geom_col()
+   })
+ 
+ 
+ output$id.Graf.Divisao <-
+   renderPlot({
+     ggplot(data = df_Divisao(),
+            mapping = aes(x = divisao, y = estoque)) +
+       geom_col()
+   })
+
+ 
   
+ 
   
-  # df <-
-  #   eventReactive(input$id.Atualizar.button, {
-  #     df_long_cnae[df_long_cnae$trimestre %in% input$id.Trimestre.select, ]
-  #   }, ignoreNULL = F)
-  # 
-  # output$id.Graf.Trimestre <-
-  #   renderPlot({
-  #     ggplot(data = df(),
-  #            mapping = aes(x = trimestre, y = estoque)) +
-  #       geom_col()
-  #   })
+ output$id.Tb.Divisao <- 
+   renderReactable(
+     reactable(df_Divisao_Wide()
+               ,filterable = T
+               ,compact = T
+               ,striped = T
+               ,sortable = T
+               )
+   )
+   
+   
+   
+ 
+ 
 })
+
+
+
+
+
+
+
+
+
+
+
+
